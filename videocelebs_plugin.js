11f22c8d9817c8fdd1fdc29e77b1024126400032
Tạ Quang Hiếu <failure.to.success.x@gmail.com>
2026-08-27 01:19:57 +0700

    Update videocelebs_plugin.js

--- videocelebs_plugin.js
+++ videocelebs_plugin.js
@@ -10,7 +10,7 @@ function getManifest() {
         "id": "videocelebs",
         "name": "VideoCelebs",
         "description": "Kho cảnh nhạy cảm & người nổi tiếng VideoCelebs.net (HD Stream)",
-        "version": "1.0.0",
+        "version": "1.0.1",
         "baseUrl": BASEURL,
         "iconUrl": "https://videocelebs.net/images/new2/logo.png",
         "isEnabled": true,
@@ -45,12 +45,25 @@ function getFilterConfig() {
 
 function getCachedCategories() {
     return [
-        { name: "Nude", slug: "/tags/nude" },
-        { name: "Sex", slug: "/tags/sex" },
-        { name: "Topless", slug: "/tags/topless" },
-        { name: "Butt", slug: "/tags/butt" },
-        { name: "Explicit", slug: "/tags/explicit" },
-        { name: "Lesbian", slug: "/tags/lesbian" }
+        { name: "Nude", slug: "/tag/nude" },
+        { name: "Topless", slug: "/tag/topless" },
+        { name: "Sex", slug: "/tag/sex" },
+        { name: "Butt", slug: "/tag/butt" },
+        { name: "Sexy", slug: "/tag/sexy" },
+        { name: "Underwear", slug: "/tag/underwear" },
+        { name: "Full Frontal", slug: "/tag/full-frontal" },
+        { name: "Bush", slug: "/tag/bush" },
+        { name: "Cleavage", slug: "/tag/cleavage" },
+        { name: "Bikini", slug: "/tag/bikini" },
+        { name: "Side Boob", slug: "/tag/side-boob" },
+        { name: "Lesbian", slug: "/tag/lesbian" },
+        { name: "See Thru", slug: "/tag/see-thru" },
+        { name: "Thong", slug: "/tag/thong" },
+        { name: "Explicit", slug: "/tag/explicit" },
+        { name: "Nipslip", slug: "/tag/nipslip" },
+        { name: "Implied Nudity", slug: "/tag/implied-nudity" },
+        { name: "Striptease", slug: "/tag/striptease" },
+        { name: "Nude Debut", slug: "/tag/nude-debut" }
     ];
 }
 
@@ -75,20 +88,40 @@ function getUrlList(slug, filtersJson) {
 
                 if (filters.category) {
                     if (Array.isArray(filters.category) && filters.category.length > 0) {
-                        path = filters.category[0].slug || path;
+                        path = filters.category[0].slug || filters.category[0].value || path;
                     } else if (typeof filters.category === "string") {
                         path = filters.category;
+                    } else if (filters.category.slug) {
+                        path = filters.category.slug;
+                    } else if (filters.category.value) {
+                        path = filters.category.value;
+                    }
+                } else if (filters.genre) {
+                    if (Array.isArray(filters.genre) && filters.genre.length > 0) {
+                        path = filters.genre[0].slug || filters.genre[0].value || path;
+                    } else if (typeof filters.genre === "string") {
+                        path = filters.genre;
+                    } else if (filters.genre.slug) {
+                        path = filters.genre.slug;
                     }
                 } else if (filters.sort) {
-                    var sVal = typeof filters.sort === "string" ? filters.sort : (filters.sort[0] ? filters.sort[0].value : "");
+                    var sVal = typeof filters.sort === "string" ? filters.sort : (filters.sort[0] ? (filters.sort[0].value || filters.sort[0].slug) : (filters.sort.value || filters.sort.slug || ""));
                     if (sVal) path = sVal;
                 }
             }
         }
 
+        path = path.replace(/\/+$/, "");
+        if (path === "") path = "/";
+
         if (page > 1) {
-            if (path.charAt(path.length - 1) !== '/') path += '/';
-            path += "page/" + page + "/";
+            if (path === "/") {
+                path = "/page/" + page;
+            } else if (path.indexOf("?") > -1) {
+                path += "&page=" + page;
+            } else {
+                path = path + "/page/" + page;
+            }
         }
 
         var url = path;
@@ -114,11 +147,11 @@ function getUrlSearch(keyword, filtersJson) {
         }
         if (filters && filters.page) page = parseInt(filters.page, 10) || 1;
     }
-    var safeKeyword = encodeURIComponent(keyword || "").replace(/%20/g, "+");
+    var cleanKeyword = (keyword || "").trim();
     if (page > 1) {
-        return BASEURL + "/page/" + page + "/?s=" + safeKeyword;
+        return BASEURL + "/search/" + encodeURIComponent(cleanKeyword) + "/page/" + page;
     }
-    return BASEURL + "/?s=" + safeKeyword;
+    return BASEURL + "/search/" + encodeURIComponent(cleanKeyword);
 }
 
 function getUrlDetail(slug) {
@@ -148,44 +181,89 @@ function parseListResponse(html, url) {
         var items = [];
         var seen = {};
 
-        var regex = /<a[^>]+href=["'](https:\/\/videocelebs\.net\/[^"']+\.html)["'][^>]*>([\s\S]*?)<\/a>/gi;
+        var contentHtml = html;
+        var midleIndex = html.indexOf('midle_div');
+        if (midleIndex !== -1) {
+            contentHtml = html.substring(midleIndex);
+        }
+
+        var itemRegex = /<div class="item[^"]*">([\s\S]*?)<\/div>\s*<\/div>/gi;
         var match;
 
-        while ((match = regex.exec(html)) !== null) {
-            var href = match[1];
-            var inner = match[2];
+        while ((match = itemRegex.exec(contentHtml)) !== null) {
+            var inner = match[1];
+            var linkMatch = inner.match(/<a[^>]+href=["']([^"']+\.html)["']/i);
+            if (!linkMatch) continue;
 
+            var href = linkMatch[1];
             if (seen[href] || href.indexOf("/embed/") > -1) continue;
 
-            var imgMatch = inner.match(/src=["']([^"']+)["']/i) || inner.match(/data-src=["']([^"']+)["']/i);
-            var titleMatch = inner.match(/alt=["']([^"']+)["']/i) || inner.match(/title=["']([^"']+)["']/i);
+            var imgMatch = inner.match(/<img[^>]+src=["']([^"']+)["']/i) || inner.match(/data-src=["']([^"']+)["']/i);
+            var titleMatch = inner.match(/<h2[^>]*><a[^>]*>([\s\S]*?)<\/a><\/h2>/i) ||
+                             inner.match(/alt=["']([^"']+)["']/i) ||
+                             inner.match(/title=["']([^"']+)["']/i);
 
-            var title = titleMatch ? titleMatch[1].replace(/<[^>]+>/g, '').trim() : inner.replace(/<[^>]+>/g, '').trim();
+            var title = titleMatch ? titleMatch[1].replace(/<[^>]+>/g, '').trim() : "VideoCelebs Item";
             var poster = imgMatch ? imgMatch[1] : '';
+
+            var yearMatch = title.match(/\((\d{4})\)/);
+            var year = yearMatch ? parseInt(yearMatch[1], 10) : 0;
 
             if (href && (title || poster)) {
                 seen[href] = true;
                 items.push({
                     "id": href,
-                    "title": title || "VideoCelebs Item",
+                    "title": title,
                     "posterUrl": poster,
                     "backdropUrl": poster,
                     "quality": "HD",
-                    "year": 0
+                    "year": year
                 });
+            }
+        }
+
+        if (items.length === 0) {
+            var fallbackRegex = /<a[^>]+href=["'](https:\/\/videocelebs\.net\/[^"']+\.html)["'][^>]*>([\s\S]*?)<\/a>/gi;
+            while ((match = fallbackRegex.exec(contentHtml)) !== null) {
+                var fbHref = match[1];
+                var fbInner = match[2];
+                if (seen[fbHref] || fbHref.indexOf("/embed/") > -1) continue;
+
+                var fbImgMatch = fbInner.match(/src=["']([^"']+)["']/i) || fbInner.match(/data-src=["']([^"']+)["']/i);
+                var fbTitleMatch = fbInner.match(/alt=["']([^"']+)["']/i) || fbInner.match(/title=["']([^"']+)["']/i);
+
+                var fbTitle = fbTitleMatch ? fbTitleMatch[1].replace(/<[^>]+>/g, '').trim() : fbInner.replace(/<[^>]+>/g, '').trim();
+                var fbPoster = fbImgMatch ? fbImgMatch[1] : '';
+
+                if (fbHref && (fbTitle || fbPoster)) {
+                    seen[fbHref] = true;
+                    items.push({
+                        "id": fbHref,
+                        "title": fbTitle || "VideoCelebs Item",
+                        "posterUrl": fbPoster,
+                        "backdropUrl": fbPoster,
+                        "quality": "HD",
+                        "year": 0
+                    });
+                }
             }
         }
 
         var currentPage = 1;
         var totalPages = 1;
 
-        var pM = url ? url.match(/\/page\/(\d+)/) : null;
+        var pM = url ? (url.match(/\/page\/(\d+)/) || url.match(/[?&]page=(\d+)/)) : null;
         if (pM) currentPage = parseInt(pM[1], 10);
 
-        var maxPMatch = html.match(/page\/(\d+)/g) || [];
-        for (var i = 0; i < maxPMatch.length; i++) {
-            var num = parseInt(maxPMatch[i].replace(/\D/g, ''), 10);
-            if (num && num > totalPages && num < 5000) totalPages = num;
+        var pagesMatch = html.match(/Page\s+\d+\s+of\s+(\d+)/i);
+        if (pagesMatch) {
+            totalPages = parseInt(pagesMatch[1], 10);
+        } else {
+            var maxPMatch = html.match(/page\/(\d+)/g) || [];
+            for (var i = 0; i < maxPMatch.length; i++) {
+                var num = parseInt(maxPMatch[i].replace(/\D/g, ''), 10);
+                if (num && num > totalPages && num < 10000) totalPages = num;
+            }
         }
         if (currentPage > totalPages) totalPages = currentPage;
 
